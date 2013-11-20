41b48bebe9784c40a2d4e642bad6f7cd3454b785
Akshay Bhardwaj <akshay@browserstack.com>
2013-11-20 17:04:52 +0530

    Added sample code using Minitest. Its Almost same as Unittest

---
+++ minitest/sample.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'minitest/autorun'
+require 'selenium-webdriver'
+
+class GoogleTest < MiniTest::Test
+  USERNAME = 'akshaybhardwaj1'
+  BROWSERSTACK_ACCESS_KEY = 'XQWDewaJsUzqYJRv8zhr'
+  def setup
+    if USERNAME == ''
+      puts "Please add USERNAME & BROWSERSTACK_ACCESS_KEY in this file"
+      exit
+    end
+    url = "http://#{USERNAME}:#{BROWSERSTACK_ACCESS_KEY}@hub.browserstack.com/wd/hub"
+    @driver = Selenium::WebDriver.for(:remote, :url => url)
+  end
+ 
+  def test_post
+    @driver.navigate.to "http://www.google.com"
+    element = @driver.find_element(:name, 'q')
+    element.send_keys "BrowserStack"
+    element.submit
+    assert_equal(@driver.title, "BrowserStack - Google Search")
+  end
+ 
+  def teardown
+    @driver.quit
+  end
+end
