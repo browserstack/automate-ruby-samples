5851e172c689cdd4404a24e421d837ee6c3dbdc9
Akshay Bhardwaj <akshay@browserstack.com>
2013-11-28 19:28:51 +0530

    Added speed test function in minitest framework example

--- minitest/parallel/test/sample_test.rb
+++ minitest/parallel/test/sample_test.rb
@@ -29,6 +29,19 @@ class GoogleTest < MiniTest::Test
     element.submit
     assert_equal(@driver.title, "BrowserStack - Google Search")
   end
+
+  def test_parallel
+    expected_contents = ""
+    @driver.get "http://fu.browserstack.com/browserstack_demo.html"
+    editor = @driver.find_element :id, "editor"
+    50.times do |cc|
+      puts cc
+      expected_contents << "a"
+      editor.send_keys "a"
+      actual_contents = @driver.execute_script "return document.getElementById('editor').value"
+      raise "Incorrect insertion. Expected: #{expected_contents}\nActual: #{actual_contents}" unless actual_contents == expected_contents
+    end
+  end
  
   def teardown
     @driver.quit
