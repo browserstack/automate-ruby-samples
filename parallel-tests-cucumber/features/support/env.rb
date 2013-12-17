d9def39bb906eb24fc810c58dd9553409f9f68d2
chindev <chintan@browserstack.com>
2013-12-17 20:26:32 +0530

    Use processes instead of thread because of shared ENV variables issue

--- parallel-tests-cucumber/features/support/env.rb
+++ parallel-tests-cucumber/features/support/env.rb
@@ -2,14 +2,15 @@ require 'selenium/webdriver'
 
 url = "http://#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub.browserstack.com/wd/hub"
 
-capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
-capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
-
 capabilities = Selenium::WebDriver::Remote::Capabilities.new
 capabilities['os'] = ENV['BS_AUTOMATE_OS']
 capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
 capabilities['browser'] = ENV['SELENIUM_BROWSER']
 capabilities['browser_version'] = ENV['SELENIUM_VERSION']
+capabilities['browserstack.debug'] = "true"
+
+capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
+capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
 
 browser = Selenium::WebDriver.for(:remote, :url => url,
                                   :desired_capabilities => capabilities)
